9da22feb99751bdf5df34acc8e955c1e5c2ace26
schsandor <84130289+schsandor@users.noreply.github.com>
2024-06-24 10:47:15 +0200

    Add project files.

---
+++ DissappearingFlyoutText/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DissappearingFlyoutText
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
